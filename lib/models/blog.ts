66763414096f6e8d2f1e98adaee54cf7995a4539
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2023-05-17 19:43:44 +0700

    Remove all template files

--- lib/models/blog.ts
+++
@@ -1,6 +0,0 @@
-import type { Post } from './post';
-
-export interface BlogState {
-  posts: Post[];
-  isLoading: boolean;
-}
--- lib/store/reducers.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
-import blogReducer from './slices/blog';
-
-export default combineReducers({
-  blog: blogReducer,
-});
